56d1fcaccb7e93cd82541b14ea2b7f55e1e292a9
AkronimBlack <39061674+AkronimBlack@users.noreply.github.com>
2018-08-30 17:33:06 +0200

    menu part1

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,7 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const portrait = document.querySelector('.portrait');
 
 //inital state
 let showMenu = false;
@@ -14,7 +15,9 @@ function toggleMenu() {
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
+        portrait.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
+
         //menu state
         showMenu = true;
     } else {
@@ -22,7 +25,9 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
+        portrait.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
+
         //menu state
         showMenu = false;
     }
